10d0c959e6501ab2319f07563e72ed7499f53480
Qasim Hafeez <qasimh556@gmail.com>
2021-04-04 12:19:47 +0400

    added spaceX logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import logo from "./spacex-logo.jfif";
+
 function App() {
   return (
     <div className="App">
-
+      <img
+        src={logo}
+        alt="SpaceX"
+        style={{ width: 300, display: "block", margin: "auto" }}
+      />
     </div>
   );
 }
