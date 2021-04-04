6d124200ac142389ec6adde3fd83f3b3198a0c98
Qasim Hafeez <qasimh556@gmail.com>
2021-04-04 11:18:43 +0400

    removed error - graphqlHTTP is not default export

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
 
 const app = express();
@@ -12,6 +12,6 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server running on PORT:${PORT}`));
