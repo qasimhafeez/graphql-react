6c4e1d0ab2e36243f248c970e1f62f9e3263325d
Qasim Hafeez <qasimh556@gmail.com>
2021-04-04 14:25:16 +0400

    added classNames and launchItem

---
+++ client/src/components/LaunchItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classNames from "classnames";
+
+function LaunchItem({
+  launch: { flight_number, mission_name, launch_date_local, launch_success },
+}) {
+  return (
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+          <h5>
+            Mission:{" "}
+            <span
+              className={classNames({
+                "text-success": launch_success,
+                "text-danger": !launch_success,
+              })}
+            >
+              {mission_name}
+            </span>
+          </h5>
+          <p>Date: {launch_date_local}</p>
+        </div>
+        <div className="col-md-3">
+          <button className="btn btn-secondary">Launch Details</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LaunchItem;
